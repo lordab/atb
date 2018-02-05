97cf9169ba6bd0e424ea64395ab9eb4cb538f89e
Sarthak Abraham <sarthakabraham@gmail.com>
2018-02-04 22:29:38 -0700

    added changes to account screen

--- src/views/components/DepositAccount.js
+++ src/views/components/DepositAccount.js
@@ -9,35 +9,38 @@ class DepositAccount extends Component {
     this.state = {
       activeTab: props.activeTab || 0,
       totalCurrentBalance: 0,
+      totalAvailableBalance: 0,
     }
   }
 
   render() {
-    console.log('in deposit account component', this.props.accounts)
     let accounts = []
     let count = 0
-    let total = 0
+    let totalCurrent = 0
+    let totalAvailable = 0
     _.forEach(this.props.accounts, (acc) => {
       count++
-      total = total + acc.currentBalance
+      totalCurrent = totalCurrent + acc.currentBalance
+      totalAvailable = totalAvailable + acc.availableBalance
       accounts.push(
         <tr key={_.uniqueId()}>
-        <th scope="row">{'account-'+ count}</th>
+        <th scope="row">{'Deposit Account '+ count}</th>
         <td>{acc.currentBalance}</td>
         <td>{acc.availableBalance}</td>
       </tr>)
     })
-    this.state.totalCurrentBalance = total
+    this.state.totalCurrentBalance = totalCurrent
+    this.state.totalAvailableBalance = totalAvailable
     if(_.isEmpty(this.props.accounts)) {
       return (
         <div style={{margin: "50px"}}>
-        <span>No accounts to display</span>
+        <span>No deposit accounts to display</span>
         </div>
       )
     } else {
       return (
         <div style={{marginLeft: "200px", marginTop: "60px", justifyContent: "center"}}>
-          <Table bordered>
+          <Table dark>
           <thead>
             <tr>
               <th>Accounts</th>
@@ -48,8 +51,9 @@ class DepositAccount extends Component {
           <tbody>
             {accounts}
             <tr>
-              <th>Total Current Balance</th>
-              <th>{this.state.totalCurrentBalance }</th>
+              <th>Total</th>
+              <th>{this.state.totalCurrentBalance}</th>
+              <th>{this.state.totalAvailableBalance}</th>
             </tr>
           </tbody>
         </Table>
--- src/views/components/LoanAccount.js
+++ src/views/components/LoanAccount.js
@@ -8,35 +8,38 @@ class LoanAccount extends Component {
     super(props)
     this.state = {
       activeTab: props.activeTab || 0,
-      totalCurrentBalance: 0
+      totalCurrentBalance: 0,
+      totalAvailableBalance: 0,
     }
   }
   render() {
     let accounts = []
     let count = 0
     let total = 0
+    let totalAvailable = 0
     _.forEach(this.props.accounts, (acc) => {
-      console.log('in render function', acc)
       count++
       total = total + acc.currentBalance
+      totalAvailable = totalAvailable + acc.availableBalance
       accounts.push(
         <tr key={_.uniqueId()}>
-        <th scope="row">{'account-'+ count}</th>
+        <th scope="row">{'Loan Account '+ count}</th>
         <td>{acc.currentBalance}</td>
         <td>{acc.availableBalance}</td>
       </tr>)
     })
     this.state.totalCurrentBalance = total
+    this.state.totalAvailableBalance = totalAvailable
     if(_.isEmpty(this.props.accounts)) {
       return (
         <div style={{margin: "50px"}}>
-          <span>No accounts to display</span>
+          <span>No loan accounts to display</span>
         </div>
       )
     } else {
       return (
         <div style={{marginLeft: "200px", marginTop: "60px", justifyContent: "center"}}>
-          <Table bordered>
+          <Table dark>
           <thead>
             <tr>
               <th>Accounts</th>
@@ -47,8 +50,9 @@ class LoanAccount extends Component {
           <tbody>
             {accounts}
             <tr>
-              <th>Total Current Balance</th>
+              <th>Total</th>
               <th>{this.state.totalCurrentBalance }</th>
+              <th>{this.state.totalAvailableBalance}</th>
             </tr>
           </tbody>
         </Table>
